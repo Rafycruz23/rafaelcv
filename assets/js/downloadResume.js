e30c78f5077ba688a1f833279ff8e11bcbbfc986
Rafa <rafycruz23@live.com>
2023-05-08 21:20:09 -0400

    added js function to download resume

--- assets/js/downloadResume.js
+++ assets/js/downloadResume.js
@@ -1,13 +1,13 @@
-function downloadFile(filename) {
+function downloadFile(RafaelCruzLC101) {
     var link = document.createElement('a');
       // set the download attribute to the filename
-    link.setAttribute('download', filename);
+    link.setAttribute('download', RafaelCruzLC101);
       // set the href attribute to the path of the file
-    link.setAttribute('href', 'assets/' + filename);
+    link.setAttribute('href', 'images/' + RafaelCruzLC101);
   
     // simulate a click on the link element to trigger the download
     link.click();
   }
   
   // call the downloadFile function with the name of the file to download
-  downloadFile('example.pdf');
+//   downloadFile('RafaelCruzLC101.pdf');
