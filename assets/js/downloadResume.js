49b9396acc4f140b368adcdd1c81d4a34df745e1
Rafa <rafycruz23@live.com>
2023-05-08 21:27:30 -0400

    download for mobile

--- assets/js/downloadResume.js
+++ assets/js/downloadResume.js
@@ -4,9 +4,17 @@ function downloadFile(RafaelCruzLC101) {
     link.setAttribute('download', RafaelCruzLC101);
       // set the href attribute to the path of the file
     link.setAttribute('href', 'images/' + RafaelCruzLC101);
-  
-    // simulate a click on the link element to trigger the download
-    link.click();
+    link.style.display = 'none';
+
+    if (window.navigator && window.navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+      // if user is on a mobile device, use window.open to trigger the download
+      window.open(link.getAttribute('href'));
+    } else {
+      // if user is not on a mobile device, add the link element to the DOM and simulate a click on it
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
   
   // call the downloadFile function with the name of the file to download
